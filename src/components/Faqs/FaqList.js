eb368172ffda230b1013c22e9c4d23526a5e76db
Victor Aromose <48617966+vathevitechguy@users.noreply.github.com>
2022-11-12 08:51:08 -0500

    . . .

--- src/components/Faqs/FaqList.js
+++ src/components/Faqs/FaqList.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import './FaqList.scss';
 
@@ -32,21 +31,6 @@ const FaqList = (props) => {
         />
       </div>
       {isActive && <p className="FaqList__content">{content}</p>}
-=======
-import React from 'react';
-
-const FaqList = (props) => {
-  const title = props.title;
-  const direction = props.direction;
-  const content = props.content;
-  return (
-    <div className="FaqList">
-      <div className="FaqList__heading">
-        <h4>{title}</h4>
-        <img src={`./assets/icons/${direction}.svg`} alt="" />
-      </div>
-      <p>{content}</p>
->>>>>>> 31e2bdf6854e6291abf1670d823f41674e39406f
     </div>
   );
 };
